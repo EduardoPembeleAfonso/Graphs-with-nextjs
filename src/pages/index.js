c1d057c5a989b7ff0634e69ce565b742a2ca549a
eduardoPembeleAfonso <eduardopembeleafonso@gmail.com>
2023-12-20 08:19:57 +0000

    actualizacao para o vercel

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,10 @@ import BarChart  from "./components/BarChart";
 export default function Dashboard() {
   return (
     <div className="md:max-w-[1120px] flex flex-col items-center w-full mx-auto  text-white  md:pr-[4rem] min-h-screen sm:pb-[6rem] px-3 md:px-6 lg:px-8 overflow-x-hidden">
-      {/* GRAFIC LINE AND INFO GRAPH LINE */}
+      {/* GRAFIC LINE AND INFO GRAPH LINE  */}
       <LineChart />
 
-      {/* GRAFIC BAR */}
+      {/* GRAFIC BAR  */}
       <BarChart />
     </div>
   );
